0a38ffb941d3571993d5636e8426d3d7d2ddfbb3
Adam <64394877+adam-barta@users.noreply.github.com>
2024-03-22 19:50:28 -0700

    Optimize imports and clean code

--- src/main/java/com/example/d288/entities/Division.java
+++ src/main/java/com/example/d288/entities/Division.java
@@ -7,7 +7,6 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.util.Date;
-import java.util.HashSet;
 import java.util.Set;
 
 @Entity
